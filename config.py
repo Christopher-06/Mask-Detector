4b431a3d8242bbfda60d585a24b58e2c0068b5b5
Christopher-06 <63494008+Christopher-06@users.noreply.github.com>
2020-07-08 19:50:54 +0200

    adjusting some hyper parameters

--- config.py
+++ config.py
@@ -5,11 +5,14 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-training_batch_files_size = 150
+# HYPER_PARAMETERS
+training_batch_files_size = 200
 training_save_intervall = 50
 batch_size = 20
-learning_rate = 0.0000001
-neural_network_file = 'data/detector_network_v1.pt'
+learning_rate = 0.0001
+
+
+neural_network_file = 'data/detector_network_v2.pt'
 
 cam_url = 'http://192.168.178.38:8080/video'
 
