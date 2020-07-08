0f81e7eca78b28e5f334c4ad777e83680719b2fe
Christopher-06 <63494008+Christopher-06@users.noreply.github.com>
2020-07-08 19:50:26 +0200

    commenting

--- network.py
+++ network.py
@@ -11,42 +11,48 @@ import time
 import os
 from os import path, listdir
 import config as conf
-#from facenet_pytorch import MTCNN
-
 
 if T.cuda.is_available():
     T.cuda.empty_cache()
 
+
 def save(model):
     model.cpu()
+    # save only weights & co
     T.save(model.state_dict(), conf.neural_network_file)
     model.to(conf.device)
 
 def load():
     model = MaskDetector()
     if path.isfile(conf.neural_network_file):
-        #load file if it's available
-        model.load_state_dict(T.load(conf.neural_network_file))
+        # load file if it is available
+        # only weights and stuff like that
+        model.load_state_dict(T.load(conf.neural_network_file, map_location=conf.device))
         print('Loaded data: ' + conf.neural_network_file)
     
     return model.to(conf.device)
 
 def has_mask(input_tensor):
+    # convert tensor to a simple boolean
     if T.argmax(input_tensor).item() == 1:
        return True
     return False
 
 def get_file_lists():
+    # list all training photos
     return  [(i, [1, 0]) for i in listdir('data/train/no_mask')] + [(i, [0, 1]) for i in listdir('data/train/with_mask')] 
 
 def get_batch_count():
+    # calculate total avaible batches
     files = len(listdir('data/train/no_mask')) + len(listdir('data/train/with_mask'))
     return int(files / conf.batch_size)
 
 def detect_faces(img):  
     if img is None:
+        # Error prehandling
         return ([], [])    
         
+    # load Caffe model and prepare image
     net = cv2.dnn.readNetFromCaffe('data/deploy.prototxt.txt', 'data/face_detection.caffemodel')
     (h, w) = img.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0,
@@ -56,62 +62,76 @@ def detect_faces(img):
     detections = net.forward()
     faces, boxes = [], []
     for i in range(0, detections.shape[2]):
+        # iterate through every face
         confidence = detections[0, 0, i, 2]
         if confidence > 0.5:
+            # face is ok
+            # --> get box coordinates
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")                                           
             face = img[startY:endY, startX:endX]
 
             if face is not None:
                 try:
+                    # try to resize
                     face = cv2.resize(face, (80, 100))
                     faces.append(face)
                     boxes.append((startX, startY, endX, endY))
                 except:
+                    # img slice gives some shit back
                     pass
                 
     return (faces, boxes)
 
 def make_training_batch_files():        
-    #delete old
+    # delete old
     for file in listdir('data/train/tensors/'):
         os.remove('data/train/tensors/' + file)
 
-    #make them   
+    # make them   
     data = get_file_lists()
     targets, tensors = [], []
     masked_faces, normal_faces = 0, 0
     while len(data) > 0:
+        # get one random and remove it
         index = np.random.randint(0, len(data))        
         file = data[index][0]
         target = data[index][1]
         data.pop(index)
 
-        print(f'File: {file}   Index:{len(tensors)}')
-        if '.db' in file:
-            continue
+        print(f'Index:{len(tensors)}   File: {file}')
+        
+       # if '.db' in file:
+        #    continue
 
         if T.cuda.is_available():
+            # clear cache
             T.cuda.empty_cache()
 
-        if target == [0, 1]:
-            img = cv2.imread('data/train/with_mask/' + file)
-        else:
-            img = cv2.imread('data/train/no_mask/' + file)
-
-        for face in detect_faces(img)[0]:
-            targets.append(target)
-            tensors.append(conf.transform(face))
-
+        if '.jpg' in file or '.png' in file or '.jpeg' in file:
+            # that's an image
             if target == [0, 1]:
-                masked_faces += 1
+                img = cv2.imread('data/train/with_mask/' + file)
             else:
-                normal_faces += 1
+                img = cv2.imread('data/train/no_mask/' + file)
+
+            for face in detect_faces(img)[0]:
+                targets.append(target)
+                tensors.append(conf.transform(face))
+
+                if target == [0, 1]:
+                    masked_faces += 1
+                else:
+                    normal_faces += 1
 
         if len(data) == 0 or len(targets) >= conf.training_batch_files_size:
+            # tensor_files max capacity reached
+            # or the last one was calculated
             T.save((tensors, T.Tensor(targets)), f'data/train/tensors/T{str(np.random.randint(100,1000))}.tensor')
             targets, tensors = [], []
             print(f"--> Made one. Masked: {masked_faces}   Unmasked: {normal_faces}")
+
+    # end statement
     print("finished")
     print(f'Masked: {masked_faces}   Unmasked: {normal_faces}')
 
@@ -119,7 +139,7 @@ class MaskDetector(nn.Module):
     def __init__(self):
         super(MaskDetector, self).__init__()        
 
-        #conv Layers
+        # conv Layers
         self.conv1 = nn.Conv2d(3, 10, kernel_size=3, stride=1)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=3, stride=1)
         self.conv2_bn = nn.BatchNorm2d(20)
@@ -128,29 +148,32 @@ class MaskDetector(nn.Module):
         self.conv4 = nn.Conv2d(30, 40, kernel_size=3, stride=1)
         self.conv4_bn = nn.BatchNorm2d(40)
 
+        # Drop out
+        # --> Better training performance
         self.drop1 = nn.Dropout2d(p=0.4)
 
-        #fully Connected Layers
+        # fully Connected Layers
         self.fc1 = nn.Linear(480,250)
         self.fc2 = nn.Linear(250, 50)
         self.fc3 = nn.Linear(50, 2)
 
     def forward(self, x):
-        #conv
+        # conv
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_bn(self.conv2(x)), 2))
         x = F.relu(F.max_pool2d(self.conv3_bn(self.conv3(x)), 2))
         x = F.relu(F.max_pool2d(self.conv4_bn(self.conv4(x)), 2))
 
+        # flat it
         x = self.drop1(x)
         x = x.view(-1, 480)
 
-        #Fully connected
+        # Fully connected
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        #[0, 1] --> Mask
-        #[1, 0] --> No Mask
+        # [0, 1] --> Mask
+        # [1, 0] --> No Mask
         return F.softmax(x)
 
 class Trainer():
@@ -183,7 +206,7 @@ class Trainer():
             tensors.append(tensor_file[0][index])
             targets.append(tensor_file[1][index])
 
-        #return tuple
+        # return tuple
         return (T.stack(tensors), T.stack(targets))
 
     def run(self):      
@@ -198,19 +221,23 @@ class Trainer():
                     if data is None:
                         break
 
+                    # seperate data and calculate values
                     _input = data[0].to(conf.device)
                     _output = self.model(_input)
                     _target = data[1].to(conf.device)
 
+                    # optimize network
                     self.optimizer.zero_grad()
                     loss = self.criterion(_output, _target)
                     loss.backward()
                     self.optimizer.step()
 
+                    # status statement
                     end = time.time()
                     print(f'Epoch: {e+1}/{self.epochs}   Batch Index: {i}/{total_batch_count}   Loss: {loss}   Time: {end - start}') 
                     
                     if i % conf.training_save_intervall:
+                        # save every ...
                         save(self.model)
                                             
             except KeyboardInterrupt:
@@ -253,8 +280,13 @@ class Tester:
                             wrong += 1
                             predict = 'Yes'
         
+                    # show result
                     print(f'Label: {l}   Tensor: {_output}   File: {f}   Prediction: {predict}')
-
+                    cv2.imshow(predict, face)
+                    cv2.waitKey(0)
+                    cv2.destroyAllWindows()
+                    
+        # end result
         print(f'Images Tested: {wrong + correct}   Correct: {correct}   Wrong: {wrong}   Percentage: {int(correct / float(correct + wrong) * 100.)}%')
 
 class Webcam_Agent():
