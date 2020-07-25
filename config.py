e4267c5ca764e7e3b56d5becb2d4f9fee6b9ab87
Christopher-06 <63494008+Christopher-06@users.noreply.github.com>
2020-07-25 13:40:08 +0200

    adding webcam variables

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import torch as T
 from torchvision import transforms
+import numpy as np
+import cv2
+
 
 transform = transforms.Compose([
     transforms.ToTensor()
@@ -11,9 +14,12 @@ training_save_intervall = 50
 batch_size = 20
 learning_rate = 0.0001
 
+show_webcam_images = True
+blank_image = np.ones((512,512,3))
+no_video_stream = cv2.putText(blank_image, 'No video stream', (200+0*int(512/2), int(512/2)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 5)
 
 neural_network_file = 'data/detector_network_v2.pt'
 
-cam_url = 'http://192.168.178.38:8080/video'
+webcams_filename = 'webcams.json'
 
 device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
